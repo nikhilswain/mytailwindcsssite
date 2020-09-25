924033a70d7d7ec2fe3c557bc96ec50917f3eb31
nikhilswain <nikhilswain36@gmail.com>
2020-09-25 22:28:34 +0530

    tailwincss site example

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    height: {
+      h10 : "10vh",
+      h15: "15vh",
+      h90: "90vh",
+    },
+    extend: {
+      colors: {
+        navbg : "#ece8e5",
+        primary: "#EBECE7",
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
